706acea742a5641c19ead89410907362efd284f3
Daniel Schierbeck <dasch@zendesk.com>
2013-11-13 14:24:37 +0100

    Clean up the spec

--- spec/logger_spec.rb
+++ spec/logger_spec.rb
@@ -1,46 +1,5 @@
 require 'remote_syslog/logger'
-
-require 'socket'
-require 'thread'
-
-class SyslogDaemon
-  attr_reader :port
-
-  def initialize(port)
-    @port = port
-    @queue = Queue.new
-    @server = TCPServer.new(port)
-    @messages = []
-    @thread = Thread.new do
-      loop do
-        client = @server.accept
-        while message = client.gets
-          @queue << message
-        end
-      end
-    end
-  end
-
-  def address
-    "localhost:#{port}"
-  end
-
-  def messages
-    read_messages!
-    @messages
-  end
-
-  private
-
-  def read_messages!
-    loop do
-      sleep 0.01
-      break if @queue.empty?
-      packet = SyslogProtocol.parse(@queue.pop)
-      @messages << [packet.severity_name, packet.content]
-    end
-  end
-end
+require 'syslog_daemon'
 
 describe RemoteSyslog::Logger do
   let(:port) { 2323 }
@@ -50,7 +9,7 @@ describe RemoteSyslog::Logger do
   it "sends messages to a remote syslog daemon" do
     logger.info "TESTING 1-2-3"
 
-    syslog.messages.should == [["info", "TESTING 1-2-3"]]
+    syslog.packets.map(&:content).should == ["TESTING 1-2-3"]
   end
 
   it "supports all the log levels" do
@@ -61,12 +20,12 @@ describe RemoteSyslog::Logger do
     logger.fatal("And a round thing in your face")
     logger.unknown("You get sprung, wanna pull out your tough")
 
-    syslog.messages.should == [
+    syslog.packets.map {|p| [p.severity_name, p.content] }.should == [
       ["debug", "I like big butts"],
-      ["info", "And I cannot lie"],
-      ["warn", "You other brothers can't deny"],
-      ["err", "That when a girl walks in with an itty bitty waist"],
-      ["crit", "And a round thing in your face"],
+      ["info",  "And I cannot lie"],
+      ["warn",  "You other brothers can't deny"],
+      ["err",   "That when a girl walks in with an itty bitty waist"],
+      ["crit",  "And a round thing in your face"],
       ["debug", "You get sprung, wanna pull out your tough"],
     ]
   end
---
+++ spec/syslog_daemon.rb
@@ -0,0 +1,41 @@
+require 'socket'
+require 'thread'
+
+class SyslogDaemon
+  attr_reader :port
+
+  def initialize(port)
+    @port = port
+    @queue = Queue.new
+    @server = TCPServer.new(port)
+    @packets = []
+    @thread = Thread.new do
+      loop do
+        client = @server.accept
+        while message = client.gets
+          @queue << message
+        end
+      end
+    end
+  end
+
+  def address
+    "localhost:#{port}"
+  end
+
+  def packets
+    read_packets!
+    @packets
+  end
+
+  private
+
+  def read_packets!
+    loop do
+      sleep 0.01
+      break if @queue.empty?
+      packet = SyslogProtocol.parse(@queue.pop)
+      @packets << packet
+    end
+  end
+end

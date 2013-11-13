51348a1761ba6da28cc2e56725b610833cb7b381
Daniel Schierbeck <dasch@zendesk.com>
2013-11-13 14:21:32 +0100

    Use the proper syslog protocol

--- lib/remote_syslog/logger.rb
+++ lib/remote_syslog/logger.rb
@@ -1,15 +1,39 @@
+require 'syslog_protocol'
+
 module RemoteSyslog
   class Logger
+    SEVERITIES = %w(debug info warn error fatal unknown)
+
+    SEVERITY_MAPPING = {
+      "error"   => "err",
+      "fatal"   => "crit",
+      "unknown" => "debug",
+    }
+
     def initialize(address)
       @address = address
     end
 
-    def info(message)
-      socket.write(message)
-      socket.close
+    SEVERITIES.each do |severity|
+      define_method(severity) {|*args| log(severity, *args) }
     end
 
     private
+
+    def log(severity, message)
+      packet = SyslogProtocol::Packet.new
+      packet.hostname = "localhost"
+      packet.facility = "user"
+      packet.tag = "foo"
+      packet.severity = map_severity(severity)
+      packet.content = message
+      socket.puts(packet.assemble)
+      socket.flush
+    end
+
+    def map_severity(severity)
+      SEVERITY_MAPPING.fetch(severity, severity)
+    end
 
     def socket
       @socket ||= TCPSocket.new(host, port)
--- spec/logger_spec.rb
+++ spec/logger_spec.rb
@@ -10,10 +10,14 @@ class SyslogDaemon
     @port = port
     @queue = Queue.new
     @server = TCPServer.new(port)
+    @messages = []
     @thread = Thread.new do
-      client = @server.accept
-      @queue << client.gets
-      client.close
+      loop do
+        client = @server.accept
+        while message = client.gets
+          @queue << message
+        end
+      end
     end
   end
 
@@ -22,19 +26,48 @@ class SyslogDaemon
   end
 
   def messages
-    [[:info, @queue.pop]]
+    read_messages!
+    @messages
+  end
+
+  private
+
+  def read_messages!
+    loop do
+      sleep 0.01
+      break if @queue.empty?
+      packet = SyslogProtocol.parse(@queue.pop)
+      @messages << [packet.severity_name, packet.content]
+    end
   end
 end
 
 describe RemoteSyslog::Logger do
   let(:port) { 2323 }
+  let(:syslog) { SyslogDaemon.new(port) }
+  let(:logger) { RemoteSyslog::Logger.new(syslog.address) }
 
   it "sends messages to a remote syslog daemon" do
-    syslog = SyslogDaemon.new(port)
-
-    logger = RemoteSyslog::Logger.new(syslog.address)
     logger.info "TESTING 1-2-3"
 
-    syslog.messages.should == [[:info, "TESTING 1-2-3"]]
+    syslog.messages.should == [["info", "TESTING 1-2-3"]]
+  end
+
+  it "supports all the log levels" do
+    logger.debug("I like big butts")
+    logger.info("And I cannot lie")
+    logger.warn("You other brothers can't deny")
+    logger.error("That when a girl walks in with an itty bitty waist")
+    logger.fatal("And a round thing in your face")
+    logger.unknown("You get sprung, wanna pull out your tough")
+
+    syslog.messages.should == [
+      ["debug", "I like big butts"],
+      ["info", "And I cannot lie"],
+      ["warn", "You other brothers can't deny"],
+      ["err", "That when a girl walks in with an itty bitty waist"],
+      ["crit", "And a round thing in your face"],
+      ["debug", "You get sprung, wanna pull out your tough"],
+    ]
   end
 end
